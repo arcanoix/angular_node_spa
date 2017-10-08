057cd214346d897a61b6370997b4a21dd8571b13
thavoo <gustavoh.2312@gmail.com>
2017-10-07 22:53:43 -0400

    charge dependencies node

---
+++ public/main.js
@@ -0,0 +1,7 @@
+/*
+contendrá toda la lógica del frontend,
+es donde tendremos los controladores de Angular JS y
+llamaremos via AJAX al API para pedir contenido, borrarlo, etc..
+
+*/
+
---
+++ server.js
@@ -0,0 +1,5 @@
+/*
+es nuestro fichero Node donde estará
+ la configuración del servidor y las rutas a nuestro API
+*/
+
