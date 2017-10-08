30ab6718af0e9a3a4a2f5e06e02024c8c1071b4a
thavoo <gustavoh.2312@gmail.com>
2017-10-07 23:22:42 -0400

    create controller angular

--- public/main.js
+++ public/main.js
@@ -5,3 +5,44 @@ llamaremos via AJAX al API para pedir contenido, borrarlo, etc..
 
 */
 
+angular.module('angularTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	// Cuando se cargue la pagina, pidel del API todos los TODOs
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data)
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+	// Cuando se añade un nuevo TODO, manda el texto a la APi
+	$scope.createTodo = function(){
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData ={};
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: '+ data);
+
+			});
+	};
+
+	//Borra un TODO despues de checkearlo como acabado
+	$scope.deleteTodo = function(id) {
+		$http.delete('/api/todos/' + id)
+		.success(function(data){
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data){
+			console.log('Error: ' + data);
+		});
+	};	
+}
