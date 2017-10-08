5380979d6a4deec3a27fdc45b34a9cee2ba5ba9f
thavoo <gustavoh.2312@gmail.com>
2017-10-07 23:13:57 -0400

    create every process server.js route

--- server.js
+++ server.js
@@ -31,6 +31,60 @@ var Todo = mongoose.model('Todo', {
 	text:String
 });
 
+// Rutas de nuestro API
+//GET de todos los TODOS
+app.get('/api/todos', function(req, res) {
+	Todo.find(function(err, todos) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(todos);
+	});
+});
+
+// POST que crea un TODO y devuelve todos tras la creacion
+app.post('/api/todos', function(req, res) {
+	Todo.create({
+		text:req.body.text,
+		done:false
+	}, function(err, todo) {
+		if(err){
+			res.send(err);
+		}
+	Todo.find(function(err, todos) {
+		if(err){
+			res.send(err);
+		}
+		res.json(todos);
+		});	
+	});
+});
+
+//Delete un TODO especifico y devuelve todos tras borrarlo.
+app.delete('/api/todos/:todo', function(req, res) {
+	Todo.remove({
+		_id:req.params.todo
+	}, function(err, todo) {
+		if(err){
+			res.send(err);
+		}
+
+		Todo.find(function(err, todos) {
+			if(err){
+				res.send(err);
+			}
+			res.json(todos);
+		});
+	})
+});
+
+//Carga una vista HTML simple donde ira nuestra single app page
+//Angular manejara el frontend
+
+app.get('*', function(req, res){
+	res.sendfile('/public/index.html');
+});
+
 //escucha en el puerto 8080 y corre el server
 app.listen(8080, function() {
 	console.log('App linstening on port 8080');
